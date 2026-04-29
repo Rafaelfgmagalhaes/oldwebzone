2733923d8afeaa9c0a7f45585cbf2abaff4bd2a8
Rafaelfgmagalhaes <rafaelfgmagalhaes@gmail.com>
2026-04-29 20:17:04 -0300

    correção bug

--- app.js
+++ app.js
@@ -1,45 +1,32 @@
-const data = new Date();
-    
-const dia = data.getDate();
-const mes = data.getMonth();
-const ano = data.getFullYear();
-const DSemana = data.getDay();
-const meses = [
-  'janeiro', 'fevereiro', 'março', 'abril',
+var data = new Date();
+var dia = data.getDate();
+var mes = data.getMonth();
+var ano = data.getFullYear();
+var DSemana = data.getDay();
+
+var meses = [
+  'janeiro', 'fevereiro', 'marco', 'abril',
   'maio', 'junho', 'julho', 'agosto',
   'setembro', 'outubro', 'novembro', 'dezembro'
 ];
 
-
-
-let hora = data.getHours();
-let minutos = data.getMinutes();
-let segundos = data.getSeconds();
-
-
-let hoje = function(){
+var hoje = function(){
     if(DSemana == 0){
         return 'Domingo';
     } else if(DSemana == 1){
-        return 'Segunda-feira'
+        return 'Segunda-feira';
     } else if(DSemana == 2){
-        return 'Terça-feira';
+        return 'Terca-feira';
     } else if(DSemana == 3){
-        return 'Quarta-feira'
-    }else if(DSemana == 4){
-        return 'Quinta-feira'
-    }else if(DSemana == 5){
-        return 'Sexta-feira'
-    }else if(DSemana == 6){
-        return 'Sábado'
+        return 'Quarta-feira';
+    } else if(DSemana == 4){
+        return 'Quinta-feira';
+    } else if(DSemana == 5){
+        return 'Sexta-feira';
+    } else if(DSemana == 6){
+        return 'Sabado';
     }
 };
 
-
-// console.log(`Olá, hoje é ${hoje()}, ${dia} de ${meses[mes]} de ${ano}`);
-
-
-const calendario = `Ol&aacute;, hoje &eacute; ${hoje()}, ${dia} de ${meses[mes]} de ${ano}   `;
+var calendario = "Ol&aacute;, hoje &eacute; " + hoje() + ", " + dia + " de " + meses[mes] + " de " + ano;
 document.getElementById("datadehoje").innerHTML = calendario;
-
-// console.log(`${hora}:${minutos}:${segundos}`);
